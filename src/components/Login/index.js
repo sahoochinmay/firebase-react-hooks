1714257476bc1badfe83e583da37362f1fbff8dc
sahoochinmay <sahoochinmay54@gmail.com>
2022-09-28 07:06:33 +0530

    Simple modification

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -50,7 +50,7 @@ function SignIn(props) {
 					<LockOutlinedIcon />
 				</Avatar>
 				<Typography component="h1" variant="h5">
-					Sign in
+					Sign In
        			</Typography>
 				<form className={classes.form} onSubmit={e => e.preventDefault() && false}>
 					<FormControl margin="normal" required fullWidth>
